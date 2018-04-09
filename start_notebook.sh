794383493b2a8d74d842224b9927e29d43633bd6
root <root@ngc4565dc-master-instance-q0lk5lso.novalocal>
2018-04-09 13:11:04 +0800

    add prepare env

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Check environment variables
-if [ -z "${BIGDL_HOME}" ]; then
-    echo "Please set BIGDL_HOME environment variable"
-    exit 1
-fi
+# Set environment variables
+# source activate py2bigdl
+export BIGDL_HOME=/opt/bigdl/dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist
+export SPARK_HOME=/opt/bmr/spark2
 
-if [ -z "${SPARK_HOME}" ]; then
-    echo "Please set SPARK_HOME environment variable"
-    exit 1
-fi
 
 #setup pathes
 export PYSPARK_DRIVER_PYTHON=jupyter
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir=./ --ip=* --no-browser --NotebookApp.token=''"
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir=./ --ip=* --no-browser --NotebookApp.token='' --allow-root"
 export BIGDL_JAR_NAME=`ls ${BIGDL_HOME}/lib/ | grep jar-with-dependencies.jar`
 export BIGDL_JAR="${BIGDL_HOME}/lib/$BIGDL_JAR_NAME"
 export BIGDL_PY_ZIP_NAME=`ls ${BIGDL_HOME}/lib/ | grep python-api.zip`
@@ -38,8 +33,9 @@ if [ ! -f $BIGDL_JAR ]; then
 fi
 
 ${SPARK_HOME}/bin/pyspark \
-  --master local[4] \
+  --master yarn-client \
   --driver-memory 4g \
+  --executor-memory 4g \
   --properties-file ${BIGDL_CONF} \
   --py-files ${BIGDL_PY_ZIP} \
   --jars ${BIGDL_JAR} \
