7f8bde1d2adb23c2a5c29e97e3765284eabf13c5
root <root@ng6aa4306-master-instance-x9xaydok.novalocal>
2018-04-10 23:13:56 +0800

    update config

--- notebooks/part1/tmp/prepare_env_single.sh
+++ notebooks/part1/tmp/prepare_env_single.sh
@@ -1,28 +1,17 @@
 #!/bin/bash
+
 mkdir -p /opt/bigdl/
 cd /opt/bigdl/
-mkdir ~/.pip
+python ./get-pip.py
+mkdir -p ~/.pip
 echo -e "[global]\\nindex-url = http://mirrors.aliyun.com/pypi/simple/\\n\\n[install]\\ntrusted-host=mirrors.aliyun.com\\n"  > ~/.pip/pip.conf
 
-#wget http://tutorial.bj.bcebos.com/dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist.zip -O dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist.zip
-#mkdir dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist
-#unzip -o dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist.zip -d dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist
-
-wget http://tutorial.bj.bcebos.com/Miniconda2-latest-Linux-x86_64.sh -O Miniconda2-latest-Linux-x86_64.sh
-export PATH=/root/miniconda2/bin:$PATH
-
-bash Miniconda2-latest-Linux-x86_64.sh -b
-conda create -y -n py2bigdl python=2.7
-
-echo -e "channels:\\n - https://mirrors.ustc.edu.cn/anaconda/pkgs/free/\\n - defaults\\nssl_verify: true\\nshow_channel_urls: true" > ~/.condarc
-source activate py2bigdl
 pip install keras==1.2.2
 pip install tensorflow==1.0.0
 pip install h5py
-#pip install --no-dependencies BigDL==0.5.0
-conda install -y jupyter
-conda install -y pandas
-conda install -y matplotlib
+pip install jupyter
+pip install pandas
+pip install matplotlib
 
 
 mkdir -p  ~/.keras/models
@@ -31,11 +20,9 @@ wget https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_i
 wget http://tutorial.bj.bcebos.com/resnet50_weights_th_dim_ordering_th_kernels.h5
 
 cd /opt/bigdl/
-wget http://mirrors.tuna.tsinghua.edu.cn/apache/spark/spark-1.6.3/spark-1.6.3-bin-hadoop2.6.tgz
-tar -zxvf /opt/bigdl/spark-1.6.3-bin-hadoop2.6.tgz
 
 wget http://tutorial.bj.bcebos.com/dist-spark-1.6.2-scala-2.10.5-all-0.5.0-dist.zip
-mkdir bigdl-0.5.0-spark1.6.3
+mkdir -p bigdl-0.5.0-spark1.6.3
 unzip dist-spark-1.6.2-scala-2.10.5-all-0.5.0-dist.zip -d bigdl-0.5.0-spark1.6.3
 
 
--- prepare_env.sh
+++ prepare_env.sh
@@ -2,6 +2,7 @@
 hdfs dfs -put notebooks/part4/image-net /user/root
 mkdir -p /opt/bigdl/
 cd /opt/bigdl/
+python ./get-pip.py
 mkdir ~/.pip
 echo -e "[global]\\nindex-url = http://mirrors.aliyun.com/pypi/simple/\\n\\n[install]\\ntrusted-host=mirrors.aliyun.com\\n"  > ~/.pip/pip.conf
 
@@ -9,21 +10,13 @@ wget http://tutorial.bj.bcebos.com/dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist.
 mkdir dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist
 unzip -o dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist.zip -d dist-spark-2.1.1-scala-2.11.8-all-0.5.0-dist
 
-wget http://tutorial.bj.bcebos.com/Miniconda2-latest-Linux-x86_64.sh -O Miniconda2-latest-Linux-x86_64.sh
-export PATH=/root/miniconda2/bin:$PATH
-
-bash Miniconda2-latest-Linux-x86_64.sh -b
-conda create -y -n py2bigdl python=2.7
-
-echo -e "channels:\\n - https://mirrors.ustc.edu.cn/anaconda/pkgs/free/\\n - defaults\\nssl_verify: true\\nshow_channel_urls: true" > ~/.condarc
-source activate py2bigdl
 pip install keras==1.2.2
 pip install tensorflow==1.0.0
 pip install h5py
 #pip install --no-dependencies BigDL==0.5.0
-conda install -y jupyter
-conda install -y pandas
-conda install -y matplotlib
+pip install  jupyter
+pip install  pandas
+pip install  matplotlib
 
 # download some data
 mkdir -p  ~/.keras/models
