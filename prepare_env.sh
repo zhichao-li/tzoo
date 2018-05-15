6ef423c7f5fa5eae9a53e63af8fd0981c37d6663
li,zhichao <zhichao.li@intel.com>
2018-05-15 11:00:23 +0800

    Update prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -23,4 +23,4 @@ fi
 #python ./get-pip.py
 #mkdir ~/.pip
 echo -e "[global]\\nindex-url = http://mirrors.aliyun.com/pypi/simple/\\n\\n[install]\\ntrusted-host=mirrors.aliyun.com\\n"  > ~/.pip/pip.conf
-sudo -EH pip install numpy==1.12.0 six keras==1.2.2 tensorflow==1.0.0 h5py jupyter pandas==0.22.0  matplotlib seaborn
+sudo -EH pip install numpy==1.12.0 six keras==1.2.2 tensorflow==1.1.0 h5py jupyter pandas==0.22.0  matplotlib seaborn
